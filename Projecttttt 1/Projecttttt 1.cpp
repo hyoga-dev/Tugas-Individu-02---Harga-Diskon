c4e3ac76f8baa75abfb577e8459a57455371d0ac
Lamy <hyogadecaprio@gmail.com>
2024-09-29 06:04:13 +0700

    Deletting the comment

--- Projecttttt 1/Projecttttt 1.cpp
+++ Projecttttt 1/Projecttttt 1.cpp
@@ -1,6 +1,3 @@
-// Projecttttt 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 //#include <stdio.h>
 #include <string>
@@ -14,6 +11,8 @@ string formatAngka(int angka) {
         hasil.insert(i, ".");
     }
 
+    hasil = "Rp." + hasil;
+
     return hasil;
 }
 
@@ -26,21 +25,11 @@ int main()
     float hasilSetelahDiskon = hargaBarang - hargaDiskon;
 
     //printf("Harga, %s", formatAngka(hargaBarang));
-    cout << "Harga: " << "Rp" << formatAngka(hargaBarang) << endl;
+    cout << "Harga: " << formatAngka(hargaBarang) << endl;
     cout << "Diskon: " << diskon << "%" << endl;
-    cout << "Besarnya diskon dari harga: " << "Rp" << formatAngka(hargaDiskon) << endl;
-    cout << "Harga setelah diskon: " << "Rp" << formatAngka(hasilSetelahDiskon) << endl;
+    cout << "Besarnya diskon dari harga: " << formatAngka(hargaDiskon) << endl;
+    cout << "Harga setelah diskon: " << formatAngka(hasilSetelahDiskon) << endl;
 
     return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
